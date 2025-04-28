af9b1e7e70c0ab2255b8edddf0dcb61ebb284b7f
Ma7en <m9ee9m@gmail.com>
2025-04-28 23:21:30 +0300

    edit page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 /**
  * element toggle function
  */
 
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elemToggleFunc = function (elem) {
+    elem.classList.toggle("active");
+};
 
 /**
  * navbar variables
@@ -19,15 +19,11 @@ const overlay = document.querySelector("[data-overlay]");
 const navElemArr = [navToggleBtn, overlay];
 
 for (let i = 0; i < navElemArr.length; i++) {
-
-  navElemArr[i].addEventListener("click", function () {
-    elemToggleFunc(navbar);
-    elemToggleFunc(overlay);
-  });
-
+    navElemArr[i].addEventListener("click", function () {
+        elemToggleFunc(navbar);
+        elemToggleFunc(overlay);
+    });
 }
-
-
 
 /**
  * header sticky
@@ -38,20 +34,16 @@ const header = document.querySelector("[data-header]");
 let lastScrollPosition = 0;
 
 window.addEventListener("scroll", function () {
+    let scrollPosition = window.pageYOffset;
 
-  let scrollPosition = window.pageYOffset;
+    if (scrollPosition > lastScrollPosition) {
+        header.classList.remove("active");
+    } else {
+        header.classList.add("active");
+    }
 
-  if (scrollPosition > lastScrollPosition) {
-    header.classList.remove("active");
-  } else {
-    header.classList.add("active");
-  }
-
-  lastScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition;
-
+    lastScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition;
 });
-
-
 
 /**
  * form validation
@@ -61,16 +53,12 @@ const input = document.querySelector("[data-input]");
 const submitBtn = document.querySelector("[data-submit]");
 
 input.addEventListener("input", function () {
-
-  if (input.checkValidity()) {
-    submitBtn.removeAttribute("disabled");
-  } else {
-    submitBtn.setAttribute("disabled", "");
-  }
-
+    if (input.checkValidity()) {
+        submitBtn.removeAttribute("disabled");
+    } else {
+        submitBtn.setAttribute("disabled", "");
+    }
 });
-
-
 
 /**
  * go top
@@ -79,7 +67,7 @@ input.addEventListener("input", function () {
 const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-
-  window.scrollY >= 200 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
-
+    window.scrollY >= 200
+        ? goTopBtn.classList.add("active")
+        : goTopBtn.classList.remove("active");
 });
